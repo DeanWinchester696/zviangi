05ed8a6f94db8bad0d06058c3cbe110834e71af4
DeanWinchester696 <luka.suxo123@gmail.com>
2023-09-09 15:46:11 +0400

    second commit

---
+++ Python/lesson1.py
@@ -0,0 +1,38 @@
+#name = "Luka" #ეს არის str (string) ტიპის ცვლადი
+
+#surname = "Sukhiashvili" #ეს არის str (string) ტიპის ცვლადი
+
+#age = 22 #ეს არის int (integer) მთელი რიცხვი
+
+#height = 23424.2 # ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი
+
+#knows_programing = True #True or False
+
+
+
+#isUgly = False #ჯავასკრიფტური camelcase
+
+
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+
+
+#print(name + str(age))
+
+name = "ლუკა"
+
+surname = "სუხიაშვილი"
+
+height = 1.80
+
+age = 21 
+
+x = "ჩემი სურვილია"
+
+
+
+print("ჩემი სახელია " + name + " გვარი " + surname + " სიმაღლე: "+str(height) + "  " + x + " ვისწავლო პროგრამირება.")
